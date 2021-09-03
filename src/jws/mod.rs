20eb01f597525b0062027b72d6bf9f7783fe92f0
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-corporation.com>
2021-09-03 12:49:07 +0300

    * Minor cleanup

--- src/jws/mod.rs
+++ src/jws/mod.rs
@@ -51,24 +51,6 @@ mod tests {
             "#,
         );
 
-        // TODO: Uncomment after fixing https://github.com/hyperledger/aries-askar/issues/26
-        // _sign_verify_works::<P256KeyPair>(
-        //     "did:example:alice#key-2",
-        //     r#"
-        //     {
-        //         "kty":"EC",
-        //         "d":"7TCIdt1rhThFtWcEiLnk_COEjh1ZfQhM4bW2wz-dp4A",
-        //         "crv":"P-256",
-        //         "x":"2syLh57B-dGpa0F8p1JrO6JU7UUSF6j7qL-vfk1eOoY",
-        //         "y":"BgsGtI7UPsObMRjdElxLOrgAO9JggNMjOcfzEPox18w"
-        //      }
-        //     "#,
-        //     Algorithm::Es256,
-        //     r#"
-        //     {"id":"1234567890","typ":"application/didcomm-plain+json","type":"http://example.com/protocols/lets_do_lunch/1.0/proposal","from":"did:example:alice","to":["did:example:bob"],"created_time":1516269022,"expires_time":1516385931,"body":{"messagespecificattribute":"and its value"}}
-        //     "#
-        // );
-
         _sign_works::<K256KeyPair>(
             "did:example:alice#key-3",
             r#"
@@ -85,6 +67,8 @@ mod tests {
             {"id":"1234567890","typ":"application/didcomm-plain+json","type":"http://example.com/protocols/lets_do_lunch/1.0/proposal","from":"did:example:alice","to":["did:example:bob"],"created_time":1516269022,"expires_time":1516385931,"body":{"messagespecificattribute":"and its value"}}
             "#,
         );
+
+        // TODO: add P256KeyPair test after fixing https://github.com/hyperledger/aries-askar/issues/26
 
         fn _sign_works<Key>(alice_kid: &str, alice_key: &str, alg: Algorithm, payload: &str)
         where
