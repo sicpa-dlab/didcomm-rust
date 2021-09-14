40b0aa92235c5dd48597710c1896ac9786037612
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-corporation.com>
2021-09-14 15:18:43 +0300

    * Added authcrypt tests for P256

--- src/jwe/decrypt.rs
+++ src/jwe/decrypt.rs
@@ -94,8 +94,9 @@ impl<'a, 'b> ParsedJWE<'a, 'b> {
 mod tests {
     use askar_crypto::{
         alg::{
-            aes::{A128Kw, A256CbcHs512, A256Kw, AesKey},
+            aes::{A128Kw, A256CbcHs512, A256Gcm, A256Kw, AesKey},
             chacha20::{Chacha20Key, XC20P},
+            p256::P256KeyPair,
             x25519::X25519KeyPair,
         },
         encrypt::KeyAeadInPlace,
@@ -119,8 +120,21 @@ mod tests {
             X25519KeyPair,
             AesKey<A128Kw>,
         >(
-            Some(("Alice", ALICE_ECDH_1PU_APP_B)),
-            ("bob-key-2", BOB_ECDH_1PU_APP_B),
+            Some((ALICE_KID_ECDH_1PU_APP_B, ALICE_KEY_ECDH_1PU_APP_B)),
+            (BOB_KID_ECDH_1PU_APP_B, BOB_KEY_ECDH_1PU_APP_B),
+            MSG_ECDH_1PU_APP_B,
+            PAYLOAD_ECDH_1PU_APP_B,
+        );
+
+        // from RFC: https://tools.ietf.org/html/draft-madden-jose-ecdh-1pu-04#appendix-B
+        _decrypt_works::<
+            AesKey<A256CbcHs512>,
+            Ecdh1PU<'_, X25519KeyPair>,
+            X25519KeyPair,
+            AesKey<A128Kw>,
+        >(
+            Some((ALICE_KID_ECDH_1PU_APP_B, ALICE_KEY_ECDH_1PU_APP_B)),
+            (CHARLIE_KID_ECDH_1PU_APP_B, CHARLIE_KEY_ECDH_1PU_APP_B),
             MSG_ECDH_1PU_APP_B,
             PAYLOAD_ECDH_1PU_APP_B,
         );
@@ -163,6 +177,86 @@ mod tests {
 
         _decrypt_works::<
             AesKey<A256CbcHs512>,
+            EcdhEs<'_, X25519KeyPair>,
+            X25519KeyPair,
+            AesKey<A256Kw>,
+        >(
+            None,
+            (BOB_KID_X25519_1, BOB_KEY_X25519_1),
+            MSG_ANONCRYPT_X25519_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<
+            AesKey<A256CbcHs512>,
+            EcdhEs<'_, X25519KeyPair>,
+            X25519KeyPair,
+            AesKey<A256Kw>,
+        >(
+            None,
+            (BOB_KID_X25519_2, BOB_KEY_X25519_2),
+            MSG_ANONCRYPT_X25519_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256Gcm>, EcdhEs<'_, X25519KeyPair>, X25519KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_X25519_1, BOB_KEY_X25519_1),
+            MSG_ANONCRYPT_X25519_A256GSM,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256Gcm>, EcdhEs<'_, X25519KeyPair>, X25519KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_X25519_2, BOB_KEY_X25519_2),
+            MSG_ANONCRYPT_X25519_A256GSM,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<Chacha20Key<XC20P>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_1, BOB_KEY_P256_1),
+            MSG_ANONCRYPT_P256_XC20P,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<Chacha20Key<XC20P>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_2, BOB_KEY_P256_2),
+            MSG_ANONCRYPT_P256_XC20P,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256CbcHs512>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_1, BOB_KEY_P256_1),
+            MSG_ANONCRYPT_P256_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256CbcHs512>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_2, BOB_KEY_P256_2),
+            MSG_ANONCRYPT_P256_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256Gcm>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_1, BOB_KEY_P256_1),
+            MSG_ANONCRYPT_P256_A256GSM,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256Gcm>, EcdhEs<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            None,
+            (BOB_KID_P256_2, BOB_KEY_P256_2),
+            MSG_ANONCRYPT_P256_A256GSM,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<
+            AesKey<A256CbcHs512>,
             Ecdh1PU<'_, X25519KeyPair>,
             X25519KeyPair,
             AesKey<A256Kw>,
@@ -172,6 +266,34 @@ mod tests {
             MSG_AUTHCRYPT_X25519_A256CBC,
             PAYLOAD,
         );
+
+        _decrypt_works::<
+            AesKey<A256CbcHs512>,
+            Ecdh1PU<'_, X25519KeyPair>,
+            X25519KeyPair,
+            AesKey<A256Kw>,
+        >(
+            Some((ALICE_KID_X25519_1, ALICE_PKEY_X25519_1)),
+            (BOB_KID_X25519_2, BOB_KEY_X25519_2),
+            MSG_AUTHCRYPT_X25519_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256CbcHs512>, Ecdh1PU<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            Some((ALICE_KID_P256_1, ALICE_PKEY_P256_1)),
+            (BOB_KID_P256_1, BOB_KEY_P256_1),
+            MSG_AUTHCRYPT_P256_A256CBC,
+            PAYLOAD,
+        );
+
+        _decrypt_works::<AesKey<A256CbcHs512>, Ecdh1PU<'_, P256KeyPair>, P256KeyPair, AesKey<A256Kw>>(
+            Some((ALICE_KID_P256_1, ALICE_PKEY_P256_1)),
+            (BOB_KID_P256_2, BOB_KEY_P256_2),
+            MSG_AUTHCRYPT_P256_A256CBC,
+            PAYLOAD,
+        );
+
+        /// TODO: P-384 and P-521 support after solving https://github.com/hyperledger/aries-askar/issues/10
 
         fn _decrypt_works<CE, KDF, KE, KW>(
             sender: Option<(&str, &str)>,
@@ -185,9 +307,7 @@ mod tests {
             KW: KeyWrap + FromKeyDerivation,
         {
             let res = _decrypt::<CE, KDF, KE, KW>(sender, recepient, msg);
-
             let res = res.expect("res is err");
-            println!("{:?}", std::str::from_utf8(&res));
             assert_eq!(res, payload.as_bytes());
         }
     }
@@ -248,6 +368,106 @@ mod tests {
      }
     "#;
 
+    const MSG_ANONCRYPT_X25519_A256CBC: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYXB1IjpudWxsLCJhcHYiOiJiV3ZqdDZGM2hsUzlSeE56ZVFCQVBFOGJRdnBiQnhUa3gzS0VOUEY2aTlFIiwiZXBrIjp7ImNydiI6IlgyNTUxOSIsImt0eSI6Ik9LUCIsIngiOiJ3NEpZU0dkc1BXZldkeHZSLS12R2FTTHdZX0dTRTRwVlFhUmRRMEpLU0ZJIn19",
+        "recipients":[{
+                "header":{
+                    "kid":"did:example:bob#key-x25519-1"
+                },
+                "encrypted_key":"EPhEGPspLjEvJ_v1W5zlJGfg88huhrTEUQCfKzKXmZjp6Y7Vv9Rv0mQgb7XyeZrxKHwgMo5Vxoref9ngeT17WUuHAEEPDteF"
+            },{
+                "header":{
+                    "kid":"did:example:bob#key-x25519-2"
+                },
+                "encrypted_key":"XchtfMRjcs24QNpWBk81zW74mQFR8ungyaBlpGjaOFHWf5dlCcrGvZLIT-UEY--S_UZEVknNwOOQ-lq4F5MGtkDVOpd-HoxD"
+            }],
+        "iv":"nzmtYMd1crLyY4rRWUAL1A",
+        "ciphertext":"bDM_50XL_ArVWWgpiMZO2NFFDZqc0jFBL1RFFESE_saPogffoyDEafYFYD4OlCH9yiEOIHpZZFHrgSx66xrPrkAXfl-d3Ppin2mhx0EgiV4h8yqiN1J_dQ-b_gTsP5djIj3VxMF4mkg34oIRxuaL71DQbhWgsUw-yH16KaBHkXhQnj7T4j6lQeSrP9qNYhMD0UbXcaVzT2AvmwdhRuOuI17DrfwQMVsZnh7Zh9WwJVPwUw7pto0_YpqUacq4kq3z9ZJ1pfFEstVnRwRAosjf0UCwRzCG6nw8OJYDqS3v3_2leRsjuAk-Ro4OMt5mPki0TIBeWl8JP-5rU9kGr2o7DMUtLcNoM5NHOeKiw4BgI04lFRD-azqNXJQwlBV9Uzlq",
+        "tag":"PytY0PYyjAXno1ykdMVE75LKdZA6d8yH1Ju0jZf0n8c"
+    }
+    "#;
+
+    const MSG_ANONCRYPT_X25519_A256GSM: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJBMjU2R0NNIiwiYXB1IjpudWxsLCJhcHYiOiJiV3ZqdDZGM2hsUzlSeE56ZVFCQVBFOGJRdnBiQnhUa3gzS0VOUEY2aTlFIiwiZXBrIjp7ImNydiI6IlgyNTUxOSIsImt0eSI6Ik9LUCIsIngiOiJQRUNaUGJMdGNPYUppTFZXa3drUW1GS1RFQmExRUVBRDZhWDIxLWR3VGlrIn19",
+        "recipients":[{
+                "header":{
+                    "kid":"did:example:bob#key-x25519-1"
+                },
+                "encrypted_key":"jGcdRf7qopP0eNmvoNxi1pGBoaP3aO4CuJLutWTL0mudQhgzWgageQ"
+            },{
+                "header":{
+                    "kid":"did:example:bob#key-x25519-2"
+                },
+                "encrypted_key":"VqW0WFtTA_UnMfSwe4cxMDSCwDr65eb8D_xqNufGD01gah8lGd9Glg"
+            }],
+        "iv":"o0gOvMWbBiNzkAFk",
+        "ciphertext":"X357FFJqN7dW72W4q_qFSKudBtz9bkbqOfFvANVi5JgqUdVDSGFdUvJxDnPw8EkiF-DY4tYoFaONo5vOUjZniJ9R0Qg9W77pfa-J8ZgNp_lyfu5drRyGXcel95I2F5GuD3ZOGhNkeCMF2BHiOk5hQMcGsM2aJzbSnEnWjrjd0nzFAF6YGOz5UaUxUVJQOVgtzXvvcWgoU8LfGqKe5wm66Ul-PP8OmNSvLjJow0fXg9iP7qU1dwc6Qc2o_Pu80RMNUWw_A_dGaWc_lL4UbO5K7z2oPtqmDI0GTnu4LQHOcaJ3jdy-YyI_yTcnfzrFRPgHnNOhWG0OHQKywiDW5HUVu46MryOk2unqo25UMaInSubThYB3kzAQ",
+        "tag":"_kECR4ioqoMVe9m42fdDcQ"
+    }
+    "#;
+
+    const MSG_ANONCRYPT_P256_XC20P: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJYQzIwUCIsImFwdSI6bnVsbCwiYXB2Ijoiei1McXB2VlhEYl9zR1luM21qUUxwdXUyQ1FMZXdZdVpvVFdPSVhQSDNGTSIsImVwayI6eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6IkNVMklaXzEzZ0JhR2VxczRpUm9CclJoOVBHbzBab2lEQlFSNkdHQUJWbmciLCJ5IjoiaDJUTkh1dU5STXZNZW51TDBZcDJsU1h3dzRMNUFpc0ZKSDRXMVVzSlQ5MCJ9fQ",
+        "recipients":[{
+                "header":{
+                    "kid":"did:example:bob#key-p256-1"
+                }
+                ,"encrypted_key":"scQxV9YQ4mQrUHgl6yAnBFDXNZAiIs_15bmoErUmoYm0HtuRclPoQg"
+            },{
+                "header":{
+                    "kid":"did:example:bob#key-p256-2"
+                },
+                "encrypted_key":"CqZ-HDH2j0NC-eoUueNLKyAuMQXjQyw8bJHYM2f-lxJVm3eXCdmm2g"
+            }],
+        "iv":"Vg1uyuQKrU6Kw8OJK38WCpYFxW0suAP9",
+        "ciphertext":"2nIm3xQcFR3HXbUPF1HS_D92OGVDvL0nIi6O5ol5tnMIa09NxJtbVAYIG7ZrkT9314PqXn_Rq77hgGE6FAOgO7aNYLyUJh0JCC_i2p_XOWuk20BYyBsmmRvVpg0DY3I1Lb-Vg1pT9pEy09gsMSLhbfqk0_TFJB1rcqzR8W0YZB5mX_53nMRf1ZatDEg4rDogSekWEGTBnlTNRua8-zoI4573SfgJ-ONt7Z_KbGO-sdRkmqXhfYNcbUyoMF9JSa-kraVuWHZP9hTz8-7R020EXfb4jodMWVOMMAiJYk1Cd7tetHXpLPdtuokaapofmtL_SNftAX2CB6ULf0axrHUNtvUyjAPvpgvSuvQuMrDlaXn16MQJ_q55",
+        "tag":"etLTQvKsTvF629fykLiUDg"
+    }
+    "#;
+
+    const MSG_ANONCRYPT_P256_A256CBC: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYXB1IjpudWxsLCJhcHYiOiJ6LUxxcHZWWERiX3NHWW4zbWpRTHB1dTJDUUxld1l1Wm9UV09JWFBIM0ZNIiwiZXBrIjp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4IjoiLXh2cnltSzFLNnN3R0tLZ2VERUJyQmdhSnhWSFd1R09nTU1wTWc2OFRzYyIsInkiOiJGa3hqeUdzdVlQRVZuS3lSQ1l2a3J2ejl6cno3OTFMX2dCcWtzOWc2Y2R3In19",
+        "recipients":[{
+                "header":{
+                    "kid":"did:example:bob#key-p256-1"
+                },
+                "encrypted_key":"ReussjPSY0lT5zdj_XZgBTylAxa2yBL4Sc6rM7E42p-PsDJF60sdhAj2tm9Zg6POBI09YAy44Sygdw3rbLvcKshj6M9uD3Nc"
+            },{
+                "header":{
+                    "kid":"did:example:bob#key-p256-2"
+                },
+                "encrypted_key":"s9jBWrES_9Fvnjxof3EDlDM_i3Ds3ry6pXeIOh2n8kp15L3e6CQ2yyNeEmOwuFhVwfUxuRQbM3mVlKLtySh6UtmyzSm0UZpl"
+            }],
+        "iv":"GIBpOTfWoimvyheRyaDlVQ",
+        "ciphertext":"Dk1zcI0w1zyJjaLuaWzbOOLxsaiin1yMqHo2pI--7dJpgNID4YJCbdorekPJO5dcN8JX97DimOsZZyTde5wjZGWhRzTfHwIZT4OK9--gETjifddt2JUk0Dp72lpV35Ie6_5xU8KZEBlcajO5StBWg2sWua3hEpXTq3yJDAEMTp0Sz-CKqMlQ4frqPHJapjOz010-xVIR8cmmLoVOIA2wb5pD3uK8LLYwxUJzR-Eq5dlD6k1TtDCdZQEY38AT7a3QJ4Q49fIkZ7epREn9qLobqZwiFmbnWFfGbGY_1tPZg6eXmCTzqYxqLqXFLMgNLWOHPQPO-G77z1KzzkIbBV7xGuCxofVdjXGhHGGeUh7cymg9OGtAghMLDFvCd3kXT-FS",
+        "tag":"8FKQxyMz5f7gojQNK3Jtr5xmI3-12_VA4CJteW3fH0A"
+    }
+    "#;
+
+    const MSG_ANONCRYPT_P256_A256GSM: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC1FUytBMjU2S1ciLCJlbmMiOiJBMjU2R0NNIiwiYXB1IjpudWxsLCJhcHYiOiJ6LUxxcHZWWERiX3NHWW4zbWpRTHB1dTJDUUxld1l1Wm9UV09JWFBIM0ZNIiwiZXBrIjp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4IjoidDZYaDNGTTNna21SZ2dyUTBwMElxb3RDejdNV2tPRWRwNXZRQkNwWW9ZcyIsInkiOiJfNzgzWjRHbGFkQ095VDBFSFczU3FuVldRazNocWlSX2MwVjJGMURZVzhjIn19",
+        "recipients":[{
+                    "header":{
+                        "kid":"did:example:bob#key-p256-1"
+                    },
+                    "encrypted_key":"ZDH8GpSLzcO3r1Gw-QQPGRCIP3a_LXzeiOCqgecz7-YHBnAPEyF87Q"
+                },{
+                    "header":{
+                        "kid":"did:example:bob#key-p256-2"
+                    },
+                    "encrypted_key":"J2xoNA3fLYgPVkocnXr4KafSLOhVbMZipRCEkPBuUIF4z2hpP0tdmg"
+                }],
+        "iv":"MR5gOGVOZvqHVO7n",
+        "ciphertext":"QdzPSf5z2yrNYYXgfUAe6-7K3QpppzP3rmzkrlBgmmD_ePdcY0riNvhwBm2Bt_oc0ebK6aq-TKkAoI4P__ISe2TH8RwPuc0HjSkUZtjW8Y8x-wnuED9HEX-HuEAZlM2aFuVy3R_104kbwkAeFtrTTN00ylclOr0ldTXXjTsio58j0Yqrq7Iz6VuScYkdVfJStEswfa4e3BEO_vRAKpr2jVsj6HL5RqZUpCF-DWy_pDsu-IRpYinglvLAfkT6O9g8QApLULOdO4r8TlD1ns-Jx9lLTZZLaILSJZSuzrV6kXgwIOUnyKeA6q2NNbddoxwgpxKGfJ8Bk-Fu0uC8UP_xHVZPRiUd9PyFY6e8dcFNoumu7g4iVN7L",
+        "tag":"E5tdRcTYnxaf2p5e4wgNGQ"
+    }
+    "#;
+
     const MSG_AUTHCRYPT_X25519_A256CBC: &str = r#"
     {
         "ciphertext":"MJezmxJ8DzUB01rMjiW6JViSaUhsZBhMvYtezkhmwts1qXWtDB63i4-FHZP6cJSyCI7eU-gqH8lBXO_UVuviWIqnIUrTRLaumanZ4q1dNKAnxNL-dHmb3coOqSvy3ZZn6W17lsVudjw7hUUpMbeMbQ5W8GokK9ZCGaaWnqAzd1ZcuGXDuemWeA8BerQsfQw_IQm-aUKancldedHSGrOjVWgozVL97MH966j3i9CJc3k9jS9xDuE0owoWVZa7SxTmhl1PDetmzLnYIIIt-peJtNYGdpd-FcYxIFycQNRUoFEr77h4GBTLbC-vqbQHJC1vW4O2LEKhnhOAVlGyDYkNbA4DSL-LMwKxenQXRARsKSIMn7z-ZIqTE-VCNj9vbtgR",
@@ -277,6 +497,25 @@ mod tests {
      }
     "#;
 
+    const MSG_AUTHCRYPT_P256_A256CBC: &str = r#"
+    {
+        "protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC0xUFUrQTI1NktXIiwiZW5jIjoiQTI1NkNCQy1IUzUxMiIsInNraWQiOiJkaWQ6ZXhhbXBsZTphbGljZSNrZXktcDI1Ni0xIiwiYXB1IjoiWkdsa09tVjRZVzF3YkdVNllXeHBZMlVqYTJWNUxYQXlOVFl0TVEiLCJhcHYiOiJ6LUxxcHZWWERiX3NHWW4zbWpRTHB1dTJDUUxld1l1Wm9UV09JWFBIM0ZNIiwiZXBrIjp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4IjoiX05UZFk0Nm9TM0ZtNF9tSWxtV2VGMk9HU1NsdTU1dTJDSVlWMndzdGhDZyIsInkiOiJGcEJxeWlTc0R0NW5jb3laTWFvT3ItYUxNcmNNYW10aWZTYUstRUFrX3NrIn19",
+        "recipients":[{
+            "header":{
+                "kid":"did:example:bob#key-p256-1"
+            },
+            "encrypted_key":"VokcBeiCnIR5xCpml6oXn2nEmK31hg0aYOWG0EHlgbv19g2oyOPxsxgxuDEkk-oEXj6WNfs_mebxUQLCKpbHh7tTyqnceoh-"
+        },{
+            "header":{
+                "kid":"did:example:bob#key-p256-2"
+            },
+            "encrypted_key":"LGQRr4jN-DYlR3Vj2NrJOHcT6iZ3FblERiQNd3UxpAmXF9pI9LyG0gWlE9CTEWNN_EK_yvXPg6TCmsVwoA7VNC7tj74oz66t"
+        }],
+        "iv":"7xtMiG3E7Rjhw_6JEo4S7w",
+        "ciphertext":"ebalFD4UYdpFi5p4KDjLYfgJubV5byaCV6V-4qvqF74N4OD_zAZDd7rQxWqds67VwYX2Yw9oTYe_H6WivrCKieHFjqC01gUCbFiqS-lqe3O3WFNnwVX-WRNC7Tsha0azlulJECUXlqyKsuqp-VGYcO-OKyRfzuN_KhBbKAlfxVNxbJX-9ecb0ZqtiZRcOGvqrcDRCgQ8ApdcwGelBLs6V0bBIYnsMHkxi1eK4dMmhBpMoKR9GwQxSwBnKEs5BMO-NTRyAWWKHZXraC8nWSBivqGR9TocrD5H0xK5Ys9eUPJr453BbwTTX2BFdZQlrv4zBwHsaqsHdWPhuQyzjmQFgbmOPOtmnwSurVMRO8A1fZtbWiKy9CYqffqL8XyWmH-S","tag":"84reUHzWkfJsr6rtPMEV40gL8wswp2--V-cwmHOQt5o"
+    }
+    "#;
+
     const MSG_ECDH_1PU_APP_B: &str = r#"
     {
         "protected":"eyJhbGciOiJFQ0RILTFQVStBMTI4S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiYXB1IjoiUVd4cFkyVSIsImFwdiI6IlFtOWlJR0Z1WkNCRGFHRnliR2xsIiwiZXBrIjp7Imt0eSI6Ik9LUCIsImNydiI6IlgyNTUxOSIsIngiOiJrOW9mX2NwQWFqeTBwb1c1Z2FpeFhHczluSGt3ZzFBRnFVQUZhMzlkeUJjIn19",
@@ -302,7 +541,9 @@ mod tests {
 
     const PAYLOAD_ECDH_1PU_APP_B: &str = "Three is a magic number.";
 
-    const ALICE_ECDH_1PU_APP_B: &str = r#"
+    const ALICE_KID_ECDH_1PU_APP_B: &str = "Alice";
+
+    const ALICE_KEY_ECDH_1PU_APP_B: &str = r#"
     {
         "kty": "OKP",
         "crv": "X25519",
@@ -311,7 +552,9 @@ mod tests {
     }
     "#;
 
-    const BOB_ECDH_1PU_APP_B: &str = r#"
+    const BOB_KID_ECDH_1PU_APP_B: &str = "bob-key-2";
+
+    const BOB_KEY_ECDH_1PU_APP_B: &str = r#"
     {
         "kty": "OKP",
         "crv": "X25519",
@@ -320,7 +563,9 @@ mod tests {
     }
     "#;
 
-    const CHARLIE_ECDH_1PU_APP_B: &str = r#"
+    const CHARLIE_KID_ECDH_1PU_APP_B: &str = "2021-05-06";
+
+    const CHARLIE_KEY_ECDH_1PU_APP_B: &str = r#"
     {
         "kty": "OKP",
         "crv": "X25519",
