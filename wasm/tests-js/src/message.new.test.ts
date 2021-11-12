c96496323325e7a7f1561a454d276d53cca28ab4
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-corporation.com>
2021-11-12 17:04:41 +0300

    * Prettier default formatting

--- wasm/tests-js/src/message.new.test.ts
+++ wasm/tests-js/src/message.new.test.ts
@@ -1,42 +1,42 @@
 import { Message } from "didcomm-js";
 
-test('Message.new works', () => {
-    let msg_val = {
-        "id": "example-1",
-        "typ": "application/didcomm-plain+json",
-        "type": "example/v1",
-        "body": "example-body",
-        "from": "did:example:4",
-        "to": [
-            "did:example:1",
-            "did:example:2",
-            "did:example:3"
-        ],
-        "thid": "example-thread-1",
-        "pthid": "example-parent-thread-1",
-        "example-header-1": "example-header-1-value",
-        "example-header-2": "example-header-2-value",
-        "created_time": 10000,
-        "expires_time": 20000,
-        "attachments": [{
-            "data": {
-                "base64": "ZXhhbXBsZQ=="
-            },
-            "id": "attachment1"
-        }, {
-            "data": {
-                "json": "example"
-            },
-            "id": "attachment2"
-        }, {
-            "data": {
-                "json": "example"
-            },
-            "id": "attachment3"
-        }]
-    };
+test("Message.new works", () => {
+  let msg_val = {
+    id: "example-1",
+    typ: "application/didcomm-plain+json",
+    type: "example/v1",
+    body: "example-body",
+    from: "did:example:4",
+    to: ["did:example:1", "did:example:2", "did:example:3"],
+    thid: "example-thread-1",
+    pthid: "example-parent-thread-1",
+    "example-header-1": "example-header-1-value",
+    "example-header-2": "example-header-2-value",
+    created_time: 10000,
+    expires_time: 20000,
+    attachments: [
+      {
+        data: {
+          base64: "ZXhhbXBsZQ==",
+        },
+        id: "attachment1",
+      },
+      {
+        data: {
+          json: "example",
+        },
+        id: "attachment2",
+      },
+      {
+        data: {
+          json: "example",
+        },
+        id: "attachment3",
+      },
+    ],
+  };
 
-    let msg = new Message(msg_val);
+  let msg = new Message(msg_val);
 
-    expect(msg.as_value()).toStrictEqual(msg_val);
+  expect(msg.as_value()).toStrictEqual(msg_val);
 });
--- wasm/tests-js/src/message.pack-plaintext.test.ts
+++ wasm/tests-js/src/message.pack-plaintext.test.ts
@@ -1,11 +1,20 @@
-import { MESSAGE_SIMPLE, ExampleDIDResolver, ALICE_DID_DOC, PLAINTEXT_MSG_SIMPLE } from "./test-vectors";
+import {
+  MESSAGE_SIMPLE,
+  ExampleDIDResolver,
+  ALICE_DID_DOC,
+  PLAINTEXT_MSG_SIMPLE,
+  MESSAGE_MINIMAL,
+  PLAINTEXT_MSG_MINIMAL,
+} from "./test-vectors";
 
-test('Message.pack-plaintext works', async () => {
-    let did_resolver = new ExampleDIDResolver([ALICE_DID_DOC]);
-    let plaintext = await MESSAGE_SIMPLE.pack_plaintext(did_resolver);
+test.each([
+  [MESSAGE_SIMPLE, PLAINTEXT_MSG_SIMPLE],
+  [MESSAGE_MINIMAL, PLAINTEXT_MSG_MINIMAL],
+])("Message.pack-plaintext works", async (message, plaintext_exp) => {
+  let did_resolver = new ExampleDIDResolver([ALICE_DID_DOC]);
+  let plaintext = await message.pack_plaintext(did_resolver);
 
-    plaintext = JSON.parse(plaintext);
-    let plaintext_exp = JSON.parse(PLAINTEXT_MSG_SIMPLE);
-
-    expect(plaintext).toStrictEqual(plaintext_exp);
-})
+  plaintext = JSON.parse(plaintext);
+  plaintext_exp = JSON.parse(plaintext_exp);
+  expect(plaintext).toStrictEqual(plaintext_exp);
+});
