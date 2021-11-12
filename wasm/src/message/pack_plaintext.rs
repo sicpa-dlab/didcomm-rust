c41403a0f619b624c38321874279504635badc06
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-corporation.com>
2021-11-12 12:43:28 +0300

    JS tests infrastructure (#40)
    * Fixed wasm build in master
    * Added basic tests infrastructure for WASM

--- wasm/src/message/pack_plaintext.rs
+++ wasm/src/message/pack_plaintext.rs
@@ -1,11 +1,20 @@
+use js_sys::Promise;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::future_to_promise;
 
-use crate::{error::JsResult, Message};
+use crate::{did_resolver::JsDIDResolver, error::JsResult, DIDResolver, Message};
 
 #[wasm_bindgen]
 impl Message {
-    pub fn pack_plaintext(&self) -> Result<String, JsValue> {
-        let msg = self.0.pack_plaintext().as_js()?;
-        Ok(msg)
+    pub fn pack_plaintext(&self, did_resolver: DIDResolver) -> Promise {
+        // TODO: FIXME: think on avoid cloning
+        let msg = self.0.clone();
+        let did_resolver = JsDIDResolver(did_resolver);
+
+        future_to_promise(async move {
+            let msg = msg.pack_plaintext(&did_resolver).await.as_js()?;
+
+            Ok(msg.into())
+        })
     }
 }
