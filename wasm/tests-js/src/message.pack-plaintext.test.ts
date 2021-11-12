5a744f9d8146ccdf9bb41a4d4b2cbdf714cb834e
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-corporation.com>
2021-11-12 17:13:27 +0300

    * Better tests namings

--- wasm/tests-js/src/message.pack-plaintext.test.ts
+++ wasm/tests-js/src/message.pack-plaintext.test.ts
@@ -8,13 +8,24 @@ import {
 } from "./test-vectors";
 
 test.each([
-  [MESSAGE_SIMPLE, PLAINTEXT_MSG_SIMPLE],
-  [MESSAGE_MINIMAL, PLAINTEXT_MSG_MINIMAL],
-])("Message.pack-plaintext works", async (message, plaintext_exp) => {
-  let did_resolver = new ExampleDIDResolver([ALICE_DID_DOC]);
-  let plaintext = await message.pack_plaintext(did_resolver);
+  {
+    message: MESSAGE_SIMPLE,
+    plaintext_exp: PLAINTEXT_MSG_SIMPLE,
+    case: "Simple message",
+  },
+  {
+    message: MESSAGE_MINIMAL,
+    plaintext_exp: PLAINTEXT_MSG_SIMPLE,
+    case: "Minimal message",
+  },
+])(
+  "Message.pack-plaintext works for $case",
+  async ({ message, plaintext_exp }) => {
+    let did_resolver = new ExampleDIDResolver([ALICE_DID_DOC]);
+    let plaintext = await message.pack_plaintext(did_resolver);
 
-  plaintext = JSON.parse(plaintext);
-  plaintext_exp = JSON.parse(plaintext_exp);
-  expect(plaintext).toStrictEqual(plaintext_exp);
-});
+    plaintext = JSON.parse(plaintext);
+    plaintext_exp = JSON.parse(plaintext_exp);
+    expect(plaintext).toStrictEqual(plaintext_exp);
+  }
+);
