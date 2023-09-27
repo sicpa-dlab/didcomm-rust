d9ca021b505a429ca86455321b4fd176640512a6
Darko Kulic <darko.kulic@sicpa.com>
2023-09-27 14:23:47 +0200

    Fixed typescript linter errors

--- wasm/tests-js/src/test-vectors/kms.ts
+++ wasm/tests-js/src/test-vectors/kms.ts
@@ -1,43 +1,6 @@
 import {KeyManagementService, KnownKeyAlg} from "didcomm";
 import {ariesAskar, Ecdh1PU, Jwk, Key, KeyAlgs, SigAlgs} from "@hyperledger/aries-askar-nodejs"
-
-type SecretType =
-    "JsonWebKey2020" | "X25519KeyAgreementKey2019"
-    | "Ed25519VerificationKey2018" | "EcdsaSecp256k1VerificationKey2019" | string
-
-export class Secret {
-  /**
-   * A key ID identifying a secret (private key).
-   */
-  id: string;
-
-  /**
-   * Must have the same semantics as type ('type' field) of the corresponding method in DID Doc containing a public key.
-   */
-  type: SecretType;
-
-  /**
-   * Possible value of the secret (private key)
-   */
-  privateKeyJwk: any;
-  // privateKeyMultibase?: string;
-  // privateKeyBase58?: string;
-
-  // constructor(id: string, type: SecretType, privateKeyJwk: any) {
-  //   this.id = id;
-  //   this.type = type;
-  //   this.privateKeyJwk = privateKeyJwk;
-  // }
-  //
-  // public alg(): KnownKeyAlg {
-  //   // "Ed25519" | "X25519" | "P256" | "K256"
-  //   return "Ed25519"
-  // }
-  //
-  // public key(): Key {
-  //   return Key.fromJwk({jwk: this.privateKeyJwk })
-  // }
-}
+import { Secret} from "./secret";
 
 type KnownSignatureType =
   /// Standard signature output for ed25519
@@ -47,6 +10,7 @@ type KnownSignatureType =
   /// Elliptic curve DSA using K-256 and SHA-256
   "ES256K" | string
 
+/* tslint:disable:max-classes-per-file */
 class KidOrJwk {
   Kid?: string
   X25519Key?: string
@@ -79,8 +43,8 @@ export class ExampleKMS implements KeyManagementService {
     }
   }
 
-  async get_key_alg(secret_id: string): Promise<KnownKeyAlg> {
-    const key = this._getKey(secret_id);
+  async get_key_alg(secretId: string): Promise<KnownKeyAlg> {
+    const key = this._getKey(secretId);
     switch (key.algorithm) {
       case "ed25519":
         return "Ed25519";
@@ -101,10 +65,10 @@ export class ExampleKMS implements KeyManagementService {
     );
   }
 
-  async create_signature(secret_id: string, message: Uint8Array, sig_type: KnownSignatureType | null): Promise<Uint8Array> {
-    const key = this._getKey(secret_id);
+  async create_signature(secretId: string, message: Uint8Array, sigType: KnownSignatureType | null): Promise<Uint8Array> {
+    const key = this._getKey(secretId);
     let sigAlg = undefined
-    switch (sig_type) {
+    switch (sigType) {
       case "EdDSA":
         sigAlg = SigAlgs.EdDSA;
         break;
@@ -115,15 +79,15 @@ export class ExampleKMS implements KeyManagementService {
         sigAlg = SigAlgs.ES256K;
         break;
       default:
-        throw "Unknown signature type"
+        throw new Error("Unknown signature type")
     }
     return key.signMessage({message, sigType: sigAlg})
   }
 
-  async derive_aes_key_using_ecdh_1pu(ephem_key: KidOrJwk, send_key: KidOrJwk, recip_key: KidOrJwk, algId: Uint8Array, apu: Uint8Array, apv: Uint8Array, ccTag: Uint8Array, receive: boolean): Promise<Uint8Array> {
-    const ephemeralKey = this._resolveKey(ephem_key);
-    const recipientKey = this._resolveKey(recip_key);
-    const senderKey = this._resolveKey(send_key);
+  async derive_aes_key_using_ecdh_1pu(ephemKey: KidOrJwk, sendKey: KidOrJwk, recipKey: KidOrJwk, algId: Uint8Array, apu: Uint8Array, apv: Uint8Array, ccTag: Uint8Array, receive: boolean): Promise<Uint8Array> {
+    const ephemeralKey = this._resolveKey(ephemKey);
+    const senderKey = this._resolveKey(sendKey);
+    const recipientKey = this._resolveKey(recipKey);
 
     return new Key(ariesAskar.keyDeriveEcdh1pu({
       algorithm: KeyAlgs.AesA256Kw,
@@ -138,9 +102,9 @@ export class ExampleKMS implements KeyManagementService {
     })).secretBytes
   }
 
-  async derive_aes_key_using_ecdh_es(ephem_key: KidOrJwk, recip_key: KidOrJwk, algId: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean): Promise<Uint8Array> {
-    const ephemeralKey = this._resolveKey(ephem_key);
-    const recipientKey = this._resolveKey(recip_key);
+  async derive_aes_key_using_ecdh_es(ephemKey: KidOrJwk, recipKey: KidOrJwk, algId: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean): Promise<Uint8Array> {
+    const ephemeralKey = this._resolveKey(ephemKey);
+    const recipientKey = this._resolveKey(recipKey);
 
     const key = new Key(ariesAskar.keyDeriveEcdhEs({
       algorithm: KeyAlgs.AesA256Kw,
@@ -158,41 +122,41 @@ export class ExampleKMS implements KeyManagementService {
 
 type MockGetKeyAlg = (secretId: string) => KnownKeyAlg;
 type MockFind = (secretIds: string[]) => string[];
-type MockCreateSignature = (secret_id: string, message: Uint8Array, sig_type: KnownSignatureType | null) => Uint8Array;
-type MockDeriveECDH1PU = (ephem_key: KidOrJwk, send_key: KidOrJwk, recip_key: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, cc_tag: Uint8Array, receive: boolean) => Uint8Array
-type MockDeriveECDH1ES = (ephem_key: KidOrJwk, recip_key: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean) => Uint8Array
+type MockCreateSignature = (secretId: string, message: Uint8Array, sigType: KnownSignatureType | null) => Uint8Array;
+type MockDeriveECDH1PU = (ephemKey: KidOrJwk, sendKey: KidOrJwk, recipKey: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, ccTag: Uint8Array, receive: boolean) => Uint8Array
+type MockDeriveECDHES = (ephemKey: KidOrJwk, recipKey: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean) => Uint8Array
 
 /* tslint:disable:max-classes-per-file */
 export class MockKMS implements KeyManagementService {
   getKeyAlgHandlers: MockGetKeyAlg[];
   findHandlers: MockFind[];
   createSignatureHandlers: MockCreateSignature[];
-  createDeriveECDH_1PUHandlers: MockDeriveECDH1PU[];
-  createDeriveECDH_ESHandlers: MockDeriveECDH1ES[];
+  createDeriveECDH1PUHandlers: MockDeriveECDH1PU[];
+  createDeriveECDHESHandlers: MockDeriveECDHES[];
   fallback: KeyManagementService;
 
   constructor(
     getKeyAlgHandlers: MockGetKeyAlg[],
     findHandlers: MockFind[],
     createSignatureHandlers: MockCreateSignature[],
-    createDeriveECDH_1PUHandlers: MockDeriveECDH1PU[],
-    createDeriveECDH_ESHandlers: MockDeriveECDH1ES[],
+    createDeriveECDH1PUHandlers: MockDeriveECDH1PU[],
+    createDeriveECDHESHandlers: MockDeriveECDHES[],
     fallback: KeyManagementService
   ) {
     this.getKeyAlgHandlers = getKeyAlgHandlers;
     this.findHandlers = findHandlers;
     this.createSignatureHandlers = createSignatureHandlers;
-    this.createDeriveECDH_1PUHandlers = createDeriveECDH_1PUHandlers;
-    this.createDeriveECDH_ESHandlers = createDeriveECDH_ESHandlers;
+    this.createDeriveECDH1PUHandlers = createDeriveECDH1PUHandlers;
+    this.createDeriveECDHESHandlers = createDeriveECDHESHandlers;
     this.fallback = fallback;
   }
 
-  async get_key_alg(secret_id: string): Promise<KnownKeyAlg> {
+  async get_key_alg(secretId: string): Promise<KnownKeyAlg> {
     const handler = this.getKeyAlgHandlers.pop();
 
     return handler
-        ? handler(secret_id)
-        : await this.fallback.get_key_alg(secret_id);
+        ? handler(secretId)
+        : await this.fallback.get_key_alg(secretId);
   }
 
   async find_secrets(secretIds: string[]): Promise<string[]> {
@@ -203,27 +167,27 @@ export class MockKMS implements KeyManagementService {
       : await this.fallback.find_secrets(secretIds);
   }
 
-  async create_signature(secret_id: string, message: Uint8Array, sig_type: KnownSignatureType | null): Promise<Uint8Array> {
+  async create_signature(secretId: string, message: Uint8Array, sigType: KnownSignatureType | null): Promise<Uint8Array> {
     const handler = this.createSignatureHandlers.pop();
 
     return handler
-        ? handler(secret_id, message, sig_type)
-        : await this.fallback.create_signature(secret_id, message, sig_type);
+        ? handler(secretId, message, sigType)
+        : await this.fallback.create_signature(secretId, message, sigType);
   }
 
-  async derive_aes_key_using_ecdh_1pu(ephem_key: KidOrJwk, send_key: KidOrJwk, recip_key: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, cc_tag: Uint8Array, receive: boolean): Promise<Uint8Array> {
-    const handler = this.createDeriveECDH_1PUHandlers.pop();
+  async derive_aes_key_using_ecdh_1pu(ephemKey: KidOrJwk, sendKey: KidOrJwk, recipKey: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, ccTag: Uint8Array, receive: boolean): Promise<Uint8Array> {
+    const handler = this.createDeriveECDH1PUHandlers.pop();
 
     return handler
-        ? handler(ephem_key, send_key, recip_key, alg, apu, apv, cc_tag, receive)
-        : await this.fallback.derive_aes_key_using_ecdh_1pu(ephem_key, send_key, recip_key, alg, apu, apv, cc_tag, receive);
+        ? handler(ephemKey, sendKey, recipKey, alg, apu, apv, ccTag, receive)
+        : await this.fallback.derive_aes_key_using_ecdh_1pu(ephemKey, sendKey, recipKey, alg, apu, apv, ccTag, receive);
   }
 
-  async derive_aes_key_using_ecdh_es(ephem_key: KidOrJwk, recip_key: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean): Promise<Uint8Array> {
-    const handler = this.createDeriveECDH_ESHandlers.pop();
+  async derive_aes_key_using_ecdh_es(ephemKey: KidOrJwk, recipKey: KidOrJwk, alg: Uint8Array, apu: Uint8Array, apv: Uint8Array, receive: boolean): Promise<Uint8Array> {
+    const handler = this.createDeriveECDHESHandlers.pop();
 
     return handler
-        ? handler(ephem_key, recip_key, alg, apu, apv, receive)
-        : await this.fallback.derive_aes_key_using_ecdh_es(ephem_key, recip_key, alg, apu, apv, receive);
+        ? handler(ephemKey, recipKey, alg, apu, apv, receive)
+        : await this.fallback.derive_aes_key_using_ecdh_es(ephemKey, recipKey, alg, apu, apv, receive);
   }
 }
